43db0d0c92bd6f5eb39a6e06f8c80acc8131f772
Daniel Suess <daniel@dsuess.me>
2016-10-13 18:48:06 +0200

    Setup infrastructure

---
+++ README.md
@@ -0,0 +1,8 @@
+csalgs: Compressed sensing algorithms in Python
+===============================================
+
+This is a small collection of compressed sensing/low rank matrix recovery algorithms in Python. It's neither complete nor very elaborate -- it's mainly just for learning exisiting algorithms or for testing purposes. Use at your own risk :)
+
+## LICENSE
+
+Distributed under the terms of the GPLv3 license (see [LICENSE](LICENSE)).
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+import os
+import sys
+
+from setuptools import setup
+
+try:
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/csalgs")
+    from csalgs import __version__ as version
+except:
+    version = "unknown"
+
+
+if __name__ == '__main__':
+    setup(
+        name='csalgs',
+        author='Daniel Suess',
+        version=version,
+        packages=['csalgs'],
+        package_dir={'csalgs': 'csalgs'},
+        license="GPL",
+        install_requires=['numpy', 'cvxpy'],
+        keywords=[],
+        classifiers=[
+            "Operating System :: OS Indendent",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Intended Audience :: Science/Research"
+        ],
+        platforms=['ALL'],
+    )
