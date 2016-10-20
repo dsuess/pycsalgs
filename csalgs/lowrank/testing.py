971b5d46619cf9d0d69d29272d5de58916aba4a4
Daniel Suess <daniel@dsuess.me>
2016-10-20 15:10:49 +0200

    Refactor lowrank.testing

--- csalgs/lowrank/testing.py
+++ csalgs/lowrank/testing.py
@@ -3,28 +3,22 @@
 import numpy as np
 
 
-def random_lowrank_hmatrix(dim, rank, rgen=np.random):
-    A = rgen.randn(dim, rank)
-    return A @ A.T
-
-
-def random_lowrank_matrix(dim, rank, rgen=np.random):
-    A, B = rgen.randn(2, dim, rank)
-    return A @ B.T
+def random_lowrank_matrix(dim, rank, hermitian=False, rgen=np.random):
+    return sensingmat_rank1(1, dim, hermitian=hermitian, rgen=rgen)[0]
 
 
 def sensingmat_gauss(measurements, dim, rgen=np.random):
     return rgen.randn(measurements, dim, dim) / np.sqrt(measurements)
 
 
-def sensingmat_rank1(measurements, dim, independent=True, rgen=np.random):
+def sensingmat_rank1(measurements, dim, hermitian=True, rgen=np.random):
     A = rgen.randn(measurements, dim)
     A /= np.linalg.norm(A, axis=1, keepdims=True)
 
-    if independent:
+    if hermitian:
+        B = A
+    else:
         B = rgen.randn(measurements, dim)
         B /= np.linalg.norm(A, axis=1, keepdims=True)
-    else:
-        B = A
 
     return A[:, :, None] * B[:, None, :]
