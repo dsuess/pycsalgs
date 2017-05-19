400d40e11bbf8023bd62c1dad2394a0387a4c20b
Daniel Suess <daniel@dsuess.me>
2017-05-19 17:49:49 +1000

    Improved phase_transition script

--- examples/phase_transition.py
+++ examples/phase_transition.py
@@ -2,8 +2,9 @@
 import numpy as np
 import ipyparallel
 import pandas as pd
-from os import environ
-from tools.helpers import AsyncTaskWatcher
+from os import environ, rename
+import progressbar as pb
+from time import time
 
 
 CLUSTER_ID = environ.get('CLUSTER_ID', None)
@@ -12,12 +13,14 @@ _VIEW = _CLIENTS.load_balanced_view()
 print("Kernels available: {}".format(len(_CLIENTS)))
 
 
-SAMPLES = 1
-SITES = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-CS = np.linspace(1.0, 6.0, 10)
-RANK = 10
-DIM = 4
+SAMPLES = 100
+SITES = [2, 3, 4]
+CS = np.linspace(1.0, 2, 5)
+RANK = 5
+DIM = 2
 RGEN = np.random.RandomState(1235)
+SAVE_EVERY_SECONDS = 5
+FILENAME = 'phase_transition.pkl'
 
 
 with _CLIENTS[:].sync_imports():
@@ -27,12 +30,12 @@ with _CLIENTS[:].sync_imports():
     from mpnum.special import inner_prod_mps
 
 
-def experiment_generator(sites, dim, rank, C, dist_crit=1e-4, maxiter=25):
+def experiment_generator(sites, dim, rank, C, dist_crit=1e-4, maxiter=40):
 
     def run(seed):
         rgen = numpy.random.RandomState(seed)
         X = mpnum.random_mpa(sites, dim, rank, randstate=rgen, normalized=True)
-        nr_measurements = int(C * dim * sites * rank**2 * np.log2(rank + 1))
+        nr_measurements = int(C * dim * sites * rank**2 * numpy.log2(rank + 1))
         A = [mpnum.random_mpa(len(X), X.pdims, 1, randstate=rgen,
                               normalized=True, dtype=X.dtype)
              for _ in range(nr_measurements)]
@@ -48,21 +51,35 @@ def experiment_generator(sites, dim, rank, C, dist_crit=1e-4, maxiter=25):
 
 seeds = RGEN.randint(2**31, size=SAMPLES)
 
-watcher = AsyncTaskWatcher()
+df = pd.DataFrame(columns=['X', 'X_sharp', 'dist', 'C', 'seed', 'dim', 'rank', 'sites'])
+task_list = []
 for sites in SITES:
     for C in CS:
         sample = experiment_generator(sites, DIM, RANK, C)
         task = _VIEW.map_async(sample, seeds)
-        watcher.append(task)
-watcher.block()
+        task_list.append(task)
 
-
-df = pd.DataFrame(columns=['X', 'X_sharp', 'dist', 'C', 'seed', 'dim', 'rank', 'sites'])
 counter = 0
-for task in watcher._tasks:
-    for entry in task.get():
+last_saved = 0
+widgets = [pb.SimpleProgress(), '  [', pb.Percentage() , ']     ',
+        pb.DynamicMessage('saved@')]
+bar = pb.ProgressBar(max_value=sum(len(task) for task in task_list),
+                    widgets=widgets)
+bar.start()
+for task in task_list:
+    for entry in task:
         new_row = pd.DataFrame(entry, index=[counter])
         counter += 1
         df = df.append(new_row)
+        bar.update(value=counter)
 
-df.to_pickle('phase_transition.pkl')
+        if time() - last_saved > SAVE_EVERY_SECONDS:
+            df.to_pickle(FILENAME + '.tmp')
+            rename(FILENAME + '.tmp', FILENAME)
+            done_tasks = sum(task.progress for task in task_list)
+            # Set to current time since saving might actually take a while
+            bar.dynamic_messages = {'saved@': counter}
+            last_saved = time()
+
+df.to_pickle(FILENAME + '.tmp')
+rename(FILENAME + '.tmp', FILENAME)
